246dbf3077bffba8d0b899618d7367f5f3c9affa
WmeLuna <warriorallen11@gmail.com>
2024-07-21 13:33:38 -0400

    Init WmeMod and FarmHelper

--- src/main/java/com/wmeluna/addon/WmeMod.java
+++ src/main/java/com/wmeluna/addon/WmeMod.java
@@ -1,8 +1,8 @@
-package com.example.addon;
+package com.wmeluna.addon;
 
-import com.example.addon.commands.CommandExample;
-import com.example.addon.hud.HudExample;
-import com.example.addon.modules.ModuleExample;
+import com.wmeluna.addon.commands.CommandExample;
+import com.wmeluna.addon.hud.HudExample;
+import com.wmeluna.addon.modules.FarmHelper;
 import com.mojang.logging.LogUtils;
 import meteordevelopment.meteorclient.addons.GithubRepo;
 import meteordevelopment.meteorclient.addons.MeteorAddon;
@@ -13,17 +13,17 @@ import meteordevelopment.meteorclient.systems.modules.Category;
 import meteordevelopment.meteorclient.systems.modules.Modules;
 import org.slf4j.Logger;
 
-public class AddonTemplate extends MeteorAddon {
+public class WmeMod extends MeteorAddon {
     public static final Logger LOG = LogUtils.getLogger();
-    public static final Category CATEGORY = new Category("Example");
-    public static final HudGroup HUD_GROUP = new HudGroup("Example");
+    public static final Category CATEGORY = new Category("WmeMod");
+    public static final HudGroup HUD_GROUP = new HudGroup("WmeMod");
 
     @Override
     public void onInitialize() {
-        LOG.info("Initializing Meteor Addon Template");
+        LOG.info("Initializing WmeMod");
 
         // Modules
-        Modules.get().add(new ModuleExample());
+        Modules.get().add(new FarmHelper());
 
         // Commands
         Commands.add(new CommandExample());
@@ -39,11 +39,11 @@ public class AddonTemplate extends MeteorAddon {
 
     @Override
     public String getPackage() {
-        return "com.example.addon";
+        return "com.wmeluna.addon";
     }
 
     @Override
     public GithubRepo getRepo() {
-        return new GithubRepo("MeteorDevelopment", "meteor-addon-template");
+        return new GithubRepo("WmeLuna", "WmeMods");
     }
 }
---
+++ src/main/java/com/wmeluna/addon/modules/FarmHelper.java
@@ -0,0 +1,147 @@
+package com.wmeluna.addon.modules;
+
+import com.wmeluna.addon.WmeMod;
+import meteordevelopment.meteorclient.events.render.Render3DEvent;
+import meteordevelopment.meteorclient.events.world.TickEvent;
+import meteordevelopment.meteorclient.renderer.ShapeMode;
+import meteordevelopment.meteorclient.settings.*;
+import meteordevelopment.meteorclient.systems.modules.Module;
+import meteordevelopment.meteorclient.utils.render.color.Color;
+import meteordevelopment.meteorclient.utils.render.color.SettingColor;
+import meteordevelopment.meteorclient.utils.world.BlockIterator;
+import meteordevelopment.meteorclient.utils.misc.Pool;
+import meteordevelopment.orbit.EventHandler;
+import net.minecraft.block.*;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Box;
+import net.minecraft.util.math.Vec3d;
+
+import java.util.*;
+
+public class FarmHelper extends Module {
+    private final SettingGroup sgGeneral = this.settings.getDefaultGroup();
+    private final SettingGroup sgRender = this.settings.createGroup("Render");
+
+    /**
+     * Example setting.
+     * The {@code name} parameter should be in kebab-case.
+     * If you want to access the setting from another class, simply make the setting {@code public}, and use
+     * {@link meteordevelopment.meteorclient.systems.modules.Modules#get(Class)} to access the {@link Module} object.
+     */
+    private final Setting<List<Block>> harvestBlocks = sgGeneral.add(new BlockListSetting.Builder()
+        .name("harvest-blocks")
+        .description("Which crops to highlight")
+        .defaultValue()
+        .filter(this::harvestFilter)
+        .build()
+    );
+
+    private final Setting<SettingColor> color = sgRender.add(new ColorSetting.Builder()
+        .name("color")
+        .description("The color of the marker.")
+        .defaultValue(Color.MAGENTA)
+        .build()
+    );
+    private final Setting<Integer> range = sgGeneral.add(new IntSetting.Builder()
+        .name("range")
+        .description("Auto farm range.")
+        .defaultValue(4)
+        .min(1)
+        .build()
+    );
+
+    /**
+     * The {@code name} parameter should be in kebab-case.
+     */
+    public FarmHelper() {
+        super(WmeMod.CATEGORY, "farm-helper", "Helper for farmers");
+    }
+
+
+    private List<BlockPos> renderPos = new ArrayList<BlockPos>();
+    private final Pool<BlockPos.Mutable> blockPosPool = new Pool<>(BlockPos.Mutable::new);
+    private final List<BlockPos.Mutable> blocks = new ArrayList<>();
+
+    // @EventHandler
+    // private void onBlockUpdate(BlockUpdateEvent event) {
+    //     // int bx = event.pos.getX();
+    //     // int by = event.pos.getY();
+    //     // int bz = event.pos.getZ();
+
+    //     // if (harvestBlocks.get().contains(event.newState.getBlock()) && isMature(mc.world.getBlockState(event.pos), event.newState.getBlock()) && !renderPos.contains(event.pos)) {
+    //     //     renderPos.add(event.pos);
+    //     // }
+    //     // if (mc.world.getBlockState(event.pos).isAir() && renderPos.contains(event.pos)) {
+    //     //     renderPos.remove(event.pos);
+    //     // }
+    //     BlockPos pos = event.pos;
+    //     BlockState state = event.newState;
+    //     Block block = state.getBlock();
+    //     shouldRender(block, state, pos);
+
+    // }
+
+    @EventHandler
+    private void onRender3d(Render3DEvent event) {
+        // Render all items in list
+        renderPos.forEach(pos -> event.renderer.box(new Box(pos), color.get(), color.get(), ShapeMode.Both, 0));
+    }
+
+    @EventHandler
+    private void onTick(TickEvent.Pre event) {
+        BlockIterator.register(range.get(), range.get(), (pos, state) -> {
+            if (mc.player.getEyePos().distanceTo(Vec3d.ofCenter(pos)) <= range.get())
+                blocks.add(blockPosPool.get().set(pos));
+        });
+
+        BlockIterator.after(() -> {
+            blocks.sort(Comparator.comparingDouble(value -> mc.player.getEyePos().distanceTo(Vec3d.ofCenter(value))));
+
+            for (BlockPos pos : blocks) {
+                BlockState state = mc.world.getBlockState(pos);
+                Block block = state.getBlock();
+                shouldRender(block, state, pos);
+            }
+
+            for (BlockPos.Mutable blockPos : blocks) blockPosPool.free(blockPos);
+            blocks.clear();
+
+        });
+    }
+
+    private void shouldRender(Block block, BlockState state, BlockPos pos){
+        if (harvestBlocks.get().contains(block) && isMature(state, block) && !renderPos.contains(pos)) {
+            renderPos.add(pos);
+            // info(pos.toString() + " " + block + " " + state);
+        }
+        else if (renderPos.contains(pos)) {
+            renderPos.remove(pos);
+        }
+    }
+    private boolean harvestFilter(Block block) {
+        return block instanceof CropBlock ||
+            block == Blocks.PUMPKIN ||
+            block == Blocks.MELON ||
+            block == Blocks.NETHER_WART ||
+            block == Blocks.SWEET_BERRY_BUSH ||
+            block == Blocks.COCOA ||
+            block == Blocks.PITCHER_CROP ||
+            block == Blocks.TORCHFLOWER;
+    }
+    private boolean isMature(BlockState state, Block block) {
+        if (block instanceof CropBlock cropBlock) {
+            return cropBlock.isMature(state);
+        } else if (block instanceof CocoaBlock cocoaBlock) {
+            return state.get(cocoaBlock.AGE) >= 2;
+        } else if (block instanceof StemBlock) {
+            return state.get(StemBlock.AGE) == StemBlock.MAX_AGE;
+        } else if (block instanceof SweetBerryBushBlock sweetBerryBushBlock) {
+            return state.get(sweetBerryBushBlock.AGE) >= 2;
+        } else if (block instanceof NetherWartBlock netherWartBlock) {
+            return state.get(netherWartBlock.AGE) >= 3;
+        } else if (block instanceof PitcherCropBlock pitcherCropBlock) {
+            return state.get(pitcherCropBlock.AGE) >= 4;
+        }
+        return true;
+    }
+}
